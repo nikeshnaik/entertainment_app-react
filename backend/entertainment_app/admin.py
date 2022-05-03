a92cee7933475a5239d62cc4497fb0026478e9c0
nikeshnaik <nikeshnaik.dev@gmail.com>
2022-05-03 19:33:19 +0530

    fix: Removed Django Backend for firebase

--- backend/entertainment_app/admin.py
+++
@@ -1,9 +0,0 @@
-from textwrap import shorten
-from django.contrib import admin
-
-from .models import User, Show, UserActivity
-# Register your models here.
-
-admin.site.register(User)
-admin.site.register(Show)
-admin.site.register(UserActivity)
--- backend/entertainment_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EntertainmentAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'entertainment_app'
